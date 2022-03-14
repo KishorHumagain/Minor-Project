81f7164b76574d477f4832499fe7880eb00804ea
KishorHumagain <99068406+KishorHumagain@users.noreply.github.com>
2022-03-14 16:03:38 +0545

    Update admin.py

--- test_AQM/QPMG/admin.py
+++ test_AQM/QPMG/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .import Question,FilesAdmin
-
+from .models import Os,FilesAdmin,Dbms,Es
 # Register your models here.
-admin.site.register(Question)
 admin.site.register(FilesAdmin)
+
+
+@admin.register(Os)
+
+@admin.register(Dbms)
+
+@admin.register(Es)
+
+
+class OsAdmin(admin.ModelAdmin):
+    list_display = ('id','qn','mark')
+
+class DbmsAdmin(admin.ModelAdmin):
+    list_display = ('id','qn','mark')
+
+class EsAdmin(admin.ModelAdmin):
+    list_display = ('id','qn','mark')
