a11733c41678fd19685b4775413a2e2770539aa9
KishorHumagain <99068406+KishorHumagain@users.noreply.github.com>
2022-03-14 20:14:36 +0545

    Update views.py

--- test_AQM/AQG/views.py
+++ test_AQM/AQG/views.py
@@ -1,86 +1,141 @@
-from django.conf import settings
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User, auth
-import os
-from django.contrib import messages
-from django.http import Http404, HttpResponse
-from .models import FilesAdmin, Question
 
-# Create your views here.
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+def ocr_single(request):
 
-        user = auth.authenticate(username = username , password = password)
-
-        if user is not None:
-            auth.login(request,user)
-            return redirect('dashboard')
-        else:
-            messages.info(request,'Wrong credential!!!Ask admin for updating your details')
-            return redirect('/')
-
-        
-    else:   
-        return render(request, 'login.html' )
-
-
-
-
-
-
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
-
-
-def dashboard(request):
-    context = {
-        'file': FilesAdmin.objects.all(),
-        'user' : User.objects.all()
-    }
-    return render(request,'dashboard.html',context)
-
-def download( request ,path):
-    file_path = os.path.join(settings.MEDIA_ROOT,path)
-    if os.path.exists():
-        with open(file_path,'rb')as fh:
-            response=HttpResponse(fh.read(),content_type="application/adminupload")
-            response['content-Disposition']='inline;filename='+ os.path.basename(file_path)
-            return response
+    def extract_text_by_page(pdf_path):
+        with open(pdf_path, 'rb') as fh:
+            for page in PDFPage.get_pages(fh,
+                                        caching=True,
+                                        check_extractable=True):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(resource_manager, fake_file_handle)
+                page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                page_interpreter.process_page(page)
+            
+                text = fake_file_handle.getvalue()
+                yield text
     
-    raise Http404
+                # close open handles
+                converter.close()
+                fake_file_handle.close()
+    
+    def extract_text(pdf_path):
+        for page in extract_text_by_page(pdf_path):
+            split_text=page.split("]")
+            # print(split_text)
+            x = len(split_text)
+            for i in range(0,x):
+                texts = split_text[i]
+                patern=re.compile(r'(\d{1,3}\))\s+([a-zA-Z0-9.,:;?\'\-\|\\\s* ]+)+((\[\d\+\d)|(\[\d))')
+                matches=patern.finditer(texts)
+
+                for i in matches:
+                    grp3=i.group(3)
+                    grp3p=re.compile(r'\[(\d)+(\+)+(\d)')
+                    grp3m=grp3p.finditer(grp3)
+                    grp3pp=re.compile(r'(\[(\d)$)')
+                    grp3mm=grp3pp.finditer(grp3)
 
 
-def fullmark_validation(request):
-    full_mark = request.POST['full_mark']
-    two_mark = request.POST['two_mark']
-    four_mark = request.POST['four_mark']
-    full_mark = int(full_mark)
-    two_mark = int(two_mark)
-    four_mark = int(four_mark)
-    total_two = 2*two_mark
-    total_four = 4* four_mark
-    if(full_mark==(total_two+total_four)):
-        return render(request,'download.html')
-    else:
-        return redirect('dashboard')
+                    for j in grp3m:
+                        #group(1)=3#group(2)=+#group(3)=2
+                        grp33=j.group(0)
+                        if grp3==grp33:
+                            qn_first=re.compile('(\d{1,3}\))\s+([a-zA-Z0-9,:;\'\-\\\| ]+\?(\.)?)')
+                            qn_second=re.compile(r'(([a-zA-Z0-9,;:\'\\\|\-\ ]+)\.|\? )+(\s\[\d\+\d)')
+                            qn_first_match=qn_first.finditer(texts)
+                            qn_second_match=qn_second.finditer(texts)
+                            for i in qn_first_match:
+                                Ques=(i.group(2))#first part question
+                                marking=(j.group(1))#first part mark
+                                if subject=='os':
+                                    if Os.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Os.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
 
-def downloadfromhere(request):
-    context={
-        'file' : FilesAdmin.objects.all()
-    }
-    return render(request,'download.html',context)
+                                if subject=='dbms':
+                                    if Dbms.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Dbms.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
 
-def uploadtoadm(pdf_path):
-    if FilesAdmin.objects.filter(adminupload = pdf_path).exists():
-        new_file = FilesAdmin.objects.create(adminupload= pdf_path)
-        new_file.save()
+                                if subject=='es':
+                                    if Es.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Es.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
+
+                            for i in qn_second_match:
+                                Ques=(i.group(1))#second part question
+                                marking=(j.group(3))#second part mark
+                                if subject=='os':
+                                    if Os.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Os.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
+
+                                if subject=='dbms':
+                                    if Dbms.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Dbms.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
+
+                                if subject=='es':
+                                    if Es.objects.filter(qn=Ques).exists():
+                                        pass
+                                    else:
+                                        new_question =Es.objects.create(qn = Ques, mark = marking)
+                                        new_question.save()
+                                        
 
 
+                    for j in grp3mm:
+                        grp33=j.group(0)
+                        if grp33==grp3:
+                            Ques=(i.group(2))#sigle question 
+                            marking=(j.group(2))#single question's mark
+                            if subject=='os':
+                                if Os.objects.filter(qn=Ques).exists():
+                                    pass
+                                else:
+                                    new_question =Os.objects.create(qn = Ques, mark = marking)
+                                    new_question.save() 
 
-x = uploadtoadm('dbms_lab.pdf')
+                            if subject=='dbms':
+                                if Dbms.objects.filter(qn=Ques).exists():
+                                    pass
+                                else:
+                                    new_question =Dbms.objects.create(qn = Ques, mark = marking)
+                                    new_question.save()
+
+                            if subject=='es':
+                                if Es.objects.filter(qn=Ques).exists():
+                                    pass
+                                else:
+                                    new_question =Es.objects.create(qn = Ques, mark = marking)
+                                    new_question.save()
 
 
+        with open('test.txt','w') as f:
+            f.write(page)
+            #print()
 
+
+    filen=FilesAdmin.objects.get(id=2) 
+ 
+
+    sub=FilesAdmin.objects.all()
+    subs=[i for i in sub]
+    for i in subs:
+        subject=i.Subject
+        subject.lower()
+        print(subject)
+    
+    text=extract_text(f'{filen}')
+    return redirect(f'admin/AQG/{subject}')
